1365a1b480f5dde3570fedbfce993d8b929ff23e
Bruno Tenório <tenoriobn@gmail.com>
2024-10-09 19:06:46 -0300

    Módulo de login

---
+++ frontend/pages/auth-page-static.js
@@ -0,0 +1,12 @@
+export default function AuthPageStatic(props) {
+  return (
+    <div>
+      <h1>
+        Auth Page Static
+      </h1>
+      <pre>
+        {JSON.stringify(props, null, 2)}
+      </pre>
+    </div>
+  )
+}
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,16 +1,43 @@
+import React from 'react';
+import { useRouter } from "next/router";
+
 export default function HomeScreen() {
+  const router = useRouter();
+  const [values, setValus] = React.useState({usuario: 'omariosouto', senha: 'safepassword'});
+
+  function handleChange(event) {
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+
+    setValus((currentValues) => {
+      return {
+        ...currentValues,
+        [fieldName]: fieldValue,
+      }
+    });
+  }
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        router.push('/auth-page-ssr');
+        // router.push('/auth-page-static');
+      }}>
         <input
           placeholder="Usuário" name="usuario"
-          defaultValue="omariosouto"
+          value={values.usuario}
+          onChange={handleChange}
         />
         <input
           placeholder="Senha" name="senha" type="password"
-          defaultValue="safepassword"
+          value={values.senha}
+          onChange={handleChange}
         />
+        {/* <pre>
+          {JSON.stringify(values, null, 2)}
+        </pre> */}
         <div>
           <button>
             Entrar
